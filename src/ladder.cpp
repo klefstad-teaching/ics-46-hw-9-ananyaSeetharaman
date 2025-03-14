8d3db5110c0035badb4fe427f23ce088969599d3
Ananyashree Seetharaman <agseetha@uci.edu>
2025-03-14 04:01:33 +0000

    Fixing edit distance

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -22,32 +22,33 @@ bool edit_distance_within(const std::string& str1, const std::string& str2, int 
     int n = str1.size();
     int m = str2.size();
 
-    vector<vector<int>> dp(n + 1, vector<int>(m+1));
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1));
 
-    for (int i = 0; i <=n; ++i)
+    for (int i = 0; i <= n; ++i) 
     {
         dp[i][0] = i;
     }
 
-    for (int j = 0; j <= m; ++j)
+    for (int j = 0; j <= m; ++j) 
     {
         dp[0][j] = j;
     }
 
-    for (int i = 1; i <= n; ++i)
+    for (int i = 1; i <= n; ++i) 
     {
-        for (int j = 1; j <= m; ++j)
+        for (int j = 1; j <= m; ++j) 
         {
-            char c1 = tolower(str1[i-1]);
-            char c2 = tolower(str2[j-1]);
+            char c1 = tolower(str1[i - 1]);
+            char c2 = tolower(str2[j - 1]);
 
-            if (c1 == c2)
+            if (c1 == c2) 
             {
-                dp[i][j] = dp[i-1][j-1];
-            }
-            else
+                dp[i][j] = dp[i - 1][j - 1];
+            } 
+            
+            else 
             {
-                dp[i][j] = 1 + min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]});
+                dp[i][j] = 1 + min({dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]});
             }
         }
     }
