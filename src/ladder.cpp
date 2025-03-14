12ce66a1b50a179e9a15a75a517f7a03322de1e7
Ananyashree Seetharaman <agseetha@uci.edu>
2025-03-14 03:47:50 +0000

    Trying to call edit_distance in is_adjacent to see if my test cases are fixed and the infinite loop is fixed

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -58,39 +58,7 @@ bool edit_distance_within(const std::string& str1, const std::string& str2, int 
 
 bool is_adjacent(const string& word1, const string& word2)
 {
-    int n = word1.size();
-    int m = word2.size();
-
-    vector<vector<int>> dp(n + 1, vector<int>(m + 1));
-
-    for (int i = 0; i <= n; ++i)
-    {
-        dp[i][0] = i;
-    }
-    for (int j = 0; j <= m; ++j)
-    {
-        dp[0][j] = j;
-    }
-
-    for (int i = 1; i <= n; ++i)
-    {
-        for (int j = 1; j <= m; ++j)
-        {
-            char c1 = tolower(word1[i - 1]);
-            char c2 = tolower(word2[j - 1]);
-
-            if (c1 == c2)
-            {
-                dp[i][j] = dp[i - 1][j - 1];
-            }
-            else
-            {
-                dp[i][j] = 1 + min({ dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1] });
-            }
-        }
-    }
-    
-    return dp[n][m] <= 1;
+    return edit_distance_within(word1, word2, 1);
 }
 
 vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list)
